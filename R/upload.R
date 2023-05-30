9f2a8059898433526c0371149ad235d72dbf9d14
ferdyndongo <ferdyndongo@gmail.com>
2023-05-30 14:43:18 +0000

    add notifications for empty dsn input

--- R/upload.R
+++ R/upload.R
@@ -27,66 +27,78 @@ writeToSQLiteDSN <- function(id, dat){
 
     shiny::observeEvent(input$submit,{
       shiny::req(input$submit)
-      if(input$dbtable==""){
-        shiny::req(input$dsn, input$file_load)
-        ext <- tools::file_ext(input$file_load$name)
-        tablename <- base::switch(ext,
-                                  RDS = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
-                                  csv = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
-                                  txt = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
-                                  tsv = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
-                                  xls = stringr::str_remove_all(input$sheet,"[ ]"),
-                                  xlsx = stringr::str_remove_all(input$sheet,"[ ]"),
-                                  mdb = stringr::str_remove_all(input$sheet,"[ ]"),
-                                  db = stringr::str_remove_all(input$sheet,"[ ]"),
-                                  s3db = stringr::str_remove_all(input$sheet,"[ ]")
-        )
-        id <- shiny::showNotification("uploading to db ...", duration = NULL, closeButton = FALSE)
-        base::on.exit(shiny::removeNotification(id), add = TRUE)
-        tryCatch({
-          file_to_sqlite_dsn(input$dsn, data(), tablename, overwrite=FALSE, append=FALSE)
-        },warning=function(w){
-          shiny::showNotification(w,duration = NULL,closeButton = TRUE,type = "warning")
-          output$warning <- shinydashboard::renderMenu({
-            shinydashboard::dropdownMenu(type="notifications", badgeStatus = "warning",icon = shiny::icon("upload"),
-                                         .list=lapply(X = w,FUN = notificationItem))
+      if(input$dsn!=""){
+        if(input$dbtable==""){
+          shiny::req(input$dsn, input$file_load)
+          ext <- tools::file_ext(input$file_load$name)
+          tablename <- base::switch(ext,
+                                    RDS = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
+                                    csv = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
+                                    txt = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
+                                    tsv = stringr::str_remove_all(strsplit(x = input$file_load$name,split = ".",fixed = TRUE)[[1]][1],"[ ]"),
+                                    xls = stringr::str_remove_all(input$sheet,"[ ]"),
+                                    xlsx = stringr::str_remove_all(input$sheet,"[ ]"),
+                                    mdb = stringr::str_remove_all(input$sheet,"[ ]"),
+                                    db = stringr::str_remove_all(input$sheet,"[ ]"),
+                                    s3db = stringr::str_remove_all(input$sheet,"[ ]")
+          )
+          id <- shiny::showNotification("uploading to db ...", duration = NULL, closeButton = FALSE)
+          base::on.exit(shiny::removeNotification(id), add = TRUE)
+          tryCatch({
+            file_to_sqlite_dsn(input$dsn, data(), tablename, overwrite=FALSE, append=FALSE)
+          },warning=function(w){
+            shiny::showNotification(w,duration = NULL,closeButton = TRUE,type = "warning")
+            output$warning <- shinydashboard::renderMenu({
+              shinydashboard::dropdownMenu(type="notifications", badgeStatus = "warning",icon = shiny::icon("upload"),
+                                           .list=lapply(X = w,FUN = notificationItem))
+            })
+          },error=function(e){
+            shiny::showNotification(e,duration = NULL,closeButton = TRUE,type = "error")
+            output$warning <- shinydashboard::renderMenu({
+              shinydashboard::dropdownMenu(type="notifications", badgeStatus = "danger",icon = shiny::icon("upload"),
+                                           .list=lapply(X = e,FUN = notificationItem)
+                                           # shinydashboard::notificationItem(text = e$message,
+                                           #                                  icon = shiny::icon("upload"),
+                                           #                                  status = "danger")
+              )
+            })
           })
-        },error=function(e){
-          shiny::showNotification(e,duration = NULL,closeButton = TRUE,type = "error")
-          output$warning <- shinydashboard::renderMenu({
-            shinydashboard::dropdownMenu(type="notifications", badgeStatus = "danger",icon = shiny::icon("upload"),
-                                         .list=lapply(X = e,FUN = notificationItem)
-                                         # shinydashboard::notificationItem(text = e$message,
-                                         #                                  icon = shiny::icon("upload"),
-                                         #                                  status = "danger")
-            )
+        }else{
+          shiny::req(input$dsn, input$dbtable)
+          tablename <- stringr::str_remove_all(input$dbtable,"[ ]")
+          id <- shiny::showNotification("uploading to db ...", duration = NULL, closeButton = FALSE)
+          base::on.exit(shiny::removeNotification(id), add = TRUE)
+          tryCatch({
+            file_to_sqlite_dsn(input$dsn,data(),tablename,overwrite=FALSE,append=TRUE)
+          },warning=function(w){
+            shiny::showNotification(w,duration = NULL,closeButton = TRUE,type = "warning")
+            output$warning <- shinydashboard::renderMenu({
+              shinydashboard::dropdownMenu(type="notifications", badgeStatus = "warning",icon = shiny::icon("upload"),
+                                           .list=lapply(X = w,FUN = notificationItem))
+            })
+          },error=function(e){
+            shiny::showNotification(e,duration = NULL,closeButton = TRUE,type = "error")
+            output$warning <- shinydashboard::renderMenu({
+              shinydashboard::dropdownMenu(type="notifications", badgeStatus = "warning",icon = shiny::icon("upload"),
+                                           shinydashboard::notificationItem(text = e$message,
+                                                                            icon = shiny::icon("upload"),
+                                                                            status = "danger")
+              )
+            })
           })
-        })
-      }else{
-        shiny::req(input$dsn, input$dbtable)
-        tablename <- stringr::str_remove_all(input$dbtable,"[ ]")
-        id <- shiny::showNotification("uploading to db ...", duration = NULL, closeButton = FALSE)
-        base::on.exit(shiny::removeNotification(id), add = TRUE)
-        tryCatch({
-          file_to_sqlite_dsn(input$dsn,data(),tablename,overwrite=FALSE,append=TRUE)
-        },warning=function(w){
-          shiny::showNotification(w,duration = NULL,closeButton = TRUE,type = "warning")
-          output$warning <- shinydashboard::renderMenu({
-            shinydashboard::dropdownMenu(type="notifications", badgeStatus = "warning",icon = shiny::icon("upload"),
-                                         .list=lapply(X = w,FUN = notificationItem))
-          })
-        },error=function(e){
-          shiny::showNotification(e,duration = NULL,closeButton = TRUE,type = "error")
-          output$warning <- shinydashboard::renderMenu({
-            shinydashboard::dropdownMenu(type="notifications", badgeStatus = "warning",icon = shiny::icon("upload"),
-                                         shinydashboard::notificationItem(text = e$message,
-                                                                          icon = shiny::icon("upload"),
-                                                                          status = "danger")
-            )
-          })
-        })
 
+        }
+      }else{
+        shiny::showNotification("select the dsn where the data table will be written",duration = NULL,closeButton = TRUE,type = "error")
+        output$warning <- shinydashboard::renderMenu({
+          shinydashboard::dropdownMenu(type="notifications", badgeStatus = "warning",icon = shiny::icon("upload"),
+                                       shinydashboard::notificationItem(text = "The DSN input can't be empty",
+                                                                        icon = shiny::icon("upload"),
+                                                                        status = "danger")
+          )
+        })
       }
+
 
     })
   })
