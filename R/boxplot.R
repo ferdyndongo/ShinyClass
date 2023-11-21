7601e2bc650b78992f17d0c033194b77a0ba3047
ferdyndongo <ferdyndongo@gmail.com>
2023-11-21 10:39:05 +0000

    boxplot

--- R/boxplot.R
+++ R/boxplot.R
@@ -1,18 +1,38 @@
+#' User Interface for boxplot with inputs and outputs
+#' @param id module identifier
+boxplotUi <- function(id){
+  shiny::uiOutput(shiny::NS(id, "boxplot"))
+}
+
+numeric_plotOutput <- function(id, data) {
+  shiny::moduleServer(id, function(input, output, session) {
+
+    numVar <- shiny::reactive({
+      if(base::all(input$numVar!="")){
+        input$numVar
+      }else if(base::all(input$numVar=="")){
+        names(numericIndex(data()))
+      }
+    })
+
+    output$boxplot <- shiny::renderUI({
+      purrr::map(numVar(), shiny::plotOutput)
+    })
+
+    return(shiny::reactive(numVar()))
+  })
+}
+
 box_plot <- function(data, numVar=NULL, catVar=NULL){
   if(!(is.null(catVar) | is.null(numVar))){
-    if(all(catVar=="") & all(numVar!="")){
-      if(length(numVar)==1){
-        graphics::boxplot(data[[numVar]],horizontal=TRUE,title=paste("~", numVar))
-      }else if(length(numVar)>1){
-        graphics::boxplot(data %>% dplyr::select(dplyr::all_of(numVar)) %>% scale(),horizontal = TRUE)
-      }
-    }else if(all(catVar!="") & all(numVar!="")){
-      if(length(catVar)==1){
-        graphics::par(mfrow=c(length(numVar),1))
-        for(var in numVar){
-          graphics::boxplot(formula(paste(var,"~",catVar)),data=data,horizontal=TRUE,las=1)
-        }
-      }
+    if(all(catVar=="")){
+      # graphics::boxplot(data[[numVar]],horizontal=TRUE,main=numVar)
+      ggplot2::ggplot(data = data,mapping = ggplot2::aes(x = get(numVar))) + ggplot2::geom_boxplot(outlier.colour="red", outlier.shape=8,outlier.size=4) +
+        ggplot2::xlab(numVar)
+    }else if(all(catVar!="") && length(catVar)==1){
+      # graphics::boxplot(formula(paste(numVar,"~",catVar)),data=data,horizontal=TRUE,las=1,main=paste(numVar,"~",catVar))
+      ggplot2::ggplot(data = data,mapping = ggplot2::aes(x = get(numVar),y = get(catVar))) + ggplot2::geom_boxplot(outlier.colour="red", outlier.shape=8,outlier.size=4) +
+        ggplot2::xlab(numVar) + ggplot2::ylab(catVar)
     }
   }
 }
